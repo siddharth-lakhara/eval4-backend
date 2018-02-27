5b77a9bd69fe95fd7e1ce654ccf7ef3cc378a6dc
lakhara <siddharth.lakhara@gmail.com>
2018-02-27 21:01:43 +0530

    fixed bug

--- routes/login.js
+++ routes/login.js
@@ -7,17 +7,21 @@ module.exports = [{
     const { userName } = req.payload;
     Models.users.findOrCreate({ where: { username: userName } })
       .spread((row, bool) => {
-        if (bool) {
+        if (bool) { // a new user was created
           Models.users.findAll().then((allRow) => {
             const newUserId = allRow.length + 1;
             Models.users.update(
               { score: 0, userid: newUserId },
               { where: { username: userName } },
-            ).then((newRow) => {
-              reply(newRow.userId);
+            ).then(() => {
+              Models.responses.create({ // create empty response
+                userid: newUserId,
+                response: JSON.stringify({}),
+              });
+              reply(newUserId);
             });
           });
-        } else {
+        } else { // user already existed
           reply(row.userid);
         }
       });
